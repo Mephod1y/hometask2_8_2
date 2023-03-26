21f1f6c7e01d3ad5944c5c21761d934aed147727
Mephod1y <73156817+Mephod1y@users.noreply.github.com>
2023-03-26 17:43:20 +0300

    Update model.py

--- model.py
+++ model.py
@@ -1,5 +1,4 @@
 from mongoengine import *
-from faker import Faker
 
 connect(host="mongodb+srv://user:567234@mongodb.x4pxdoh.mongodb.net/web9", ssl=True)
 
@@ -7,27 +6,6 @@ connect(host="mongodb+srv://user:567234@mongodb.x4pxdoh.mongodb.net/web9", ssl=T
 class Contacts(Document):
     fullname = StringField(max_length=500)
     email = StringField(max_length=500)
-    logical = StringField('False')
+    sent = BooleanField(default=False)
 
 
-NUMBERS_CONTACTS = 5
-fake = Faker()
-
-
-def create_contacts():
-    contacts = [{'fullname': fake.name(), 'email': fake.email(), 'logical': 'False'} for i in range(NUMBERS_CONTACTS)]
-    return contacts
-
-
-def upload_data():
-    contacts = create_contacts()
-    for c in contacts:
-        contact = Contacts()
-        contact.fullname = c['fullname']
-        contact.email = c['email']
-        contact.logical = c['logical']
-        contact.save()
-
-
-if __name__ == '__main__':
-    upload_data()
